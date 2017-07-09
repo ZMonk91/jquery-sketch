d5507fafa042b77392729006898d5b3f131e6dd7
Zachary Monk <Zmonk91@gmail.com>
2017-07-08 20:44:39 -0700

    Updated with a color-picker

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 $(document).ready(function(){
+	// CREATE GRID
 	for (i = 0; i<1152; i++) {
 		var board = document.createElement('div');
 		board.className = 'grid';
 		document.getElementById('container').appendChild(board);
 	};
+	// END CREATE GRID
+
+	// HOVER AND CLICK FUNCTION //
 	$('.grid').hover(function(){
 		$(this).toggleClass('active');
+		$(this).click(function(){
+			$(this).addClass('changed');
+		});
 	});
+	// END HOVER AND CLICK FUNCTION
+
+	// COLOR PICKER 
+	$('#colorpicker').farbtastic('#color');
+	// END COLOR PICKER
 });
